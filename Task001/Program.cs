fe3ee374edf1ab9fc2f985ab759c955d0d3ad715
Svetlana Prokopeva <lana.prokopeva@yandex.ru>
2022-10-08 20:17:58 +0300

    Task001 added

---
+++ Task001/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// Метод нахождения чисел от 1 до N
+
+int GetSumNumbers(int value)
+{
+    if (value == 1)
+    {
+        return 1;
+    }
+    // int result = value + GetSumNumbers(value - 1);
+    // return result;
+    return value + GetSumNumbers(value - 1);
+}
+
+// Метод вывода на экран чисел от 1 до N
+
+string GetShowNumbers (int value)
+{
+     if (value == 1)
+    {
+        return "1";
+    }
+    string result = GetShowNumbers(value - 1) + "," + value;
+    return result;
+}
+
+Console.WriteLine("Введите число(N): ");
+int N = int.Parse(Console.ReadLine());
+string Numbers = GetShowNumbers(N);
+Console.WriteLine($"Числа в промежутке от 1 до N = {Numbers}");
+int sumNumbers = GetSumNumbers(N);
+Console.WriteLine($"Сумма чисел в промежутке от 1 до N = {sumNumbers}");
+
