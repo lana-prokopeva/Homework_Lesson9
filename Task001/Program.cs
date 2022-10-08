697d2504482ff922d223c7718ed4a619db329b41
Svetlana Prokopeva <lana.prokopeva@yandex.ru>
2022-10-08 20:36:29 +0300

    Task002 added

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,35 +1,24 @@
 ﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа 
 // в промежутке от N до 1. Выполнить с помощью рекурсии.
 
-// Метод нахождения чисел от 1 до N
-
-int GetSumNumbers(int value)
+// Метод вывода на экран чисел от 1 до N
+string GetShowNumbers (int value)
 {
     if (value == 1)
     {
-        return 1;
-    }
-    // int result = value + GetSumNumbers(value - 1);
-    // return result;
-    return value + GetSumNumbers(value - 1);
-}
-
-// Метод вывода на экран чисел от 1 до N
-
-string GetShowNumbers (int value)
-{
-     if (value == 1)
-    {
         return "1";
+    }
+     if (value <= 0)
+    {
+        return "Введены данные несоответствующие условиям задачи";
     }
     string result = GetShowNumbers(value - 1) + "," + value;
     return result;
 }
 
+// Определяем входные данные
 Console.WriteLine("Введите число(N): ");
 int N = int.Parse(Console.ReadLine());
 string Numbers = GetShowNumbers(N);
 Console.WriteLine($"Числа в промежутке от 1 до N = {Numbers}");
-int sumNumbers = GetSumNumbers(N);
-Console.WriteLine($"Сумма чисел в промежутке от 1 до N = {sumNumbers}");
 
---
+++ Task002/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+
+// Метод вывода на экран чисел от M до N
+string GetShowNumbers (int startValue, int endValue)
+{
+    if (startValue == endValue)
+    {
+        return $"{endValue}";
+    }
+     if (startValue > endValue)
+    {
+        return "значение M не может быть больше N!!!";
+    }
+    string result = GetShowNumbers(startValue, endValue-1) + "," + endValue;
+    return result;
+}
+
+// Определяем входные данные
+Console.WriteLine("Введите число(M): ");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число(N): ");
+int N = int.Parse(Console.ReadLine());
+string Numbers = GetShowNumbers(M, N);
+Console.WriteLine($"Числа в промежутке от M до N = {Numbers}");
+
+
